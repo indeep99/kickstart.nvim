e5a4e68fa3e78b3f9d78a5e457d11215bcf7710c
indeep99 <indeep.singh99@gmail.com>
2025-02-09 22:44:42 -0500

    add oil and add keymaping for esc

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,10 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      require('oil').setup()
+
+      vim.keymap.set('n', '-', require('oil').open, { desc = 'Open Oil File Explorer' })
+    end,
+  },
+}
